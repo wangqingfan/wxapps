01c9115e283d106de1dddd8c3b896c660a34eecb
jiaomingxin <jiaomingxin@jiaomingxindeMacBook-Pro.local>
2017-11-28 23:57:41 +0800

    测试index页

--- wxapps-web/src/main/webapp/xcx_bx/pages/index/index.js
+++ wxapps-web/src/main/webapp/xcx_bx/pages/index/index.js
@@ -7,9 +7,7 @@ Page({
   data: {
     motto: 'Hello World',
     hasData: 0,
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    userInfo: {}
   },
   //事件处理函数
   bindViewTap: function() {
@@ -20,7 +18,7 @@ Page({
   onLoad: function () {
     that = this;
     this.setData({
-      hasData: 1
+      hasData: 0
     });
     
     // 展示本地存储能力
@@ -30,52 +28,14 @@ Page({
    
   },
   onShow: function () {
-    getWxInfo();
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+    console.log(wx.canIUse('button.open-type.getUserInfo'))
+    if (app.globalData.userInfo.length > 0) {
+      console.log(app.globalData.userInfo)
+      that.setData({
+        hasData: 1
       })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
     }
-  },
-
-  
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
+    
   }
 })
-
-function getWxInfo () {
-  if (app.globalData.userInfo.length > 0) {
-    console.log(app.globalData.userInfo)
-    
-    that.setData({
-      hasData: 1
-    })
-  }
-}
