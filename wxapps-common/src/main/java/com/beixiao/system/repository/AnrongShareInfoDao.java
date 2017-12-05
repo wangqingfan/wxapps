43366d1ffb1bc2af5ca76835b29f834053f21c67
wangqingfan <m18500961204@163.com>
2017-12-06 00:52:53 +0800

    家中适应

--- wxapps-common/src/main/java/com/beixiao/system/repository/AnrongShareInfoDao.java
+++
@@ -1,8 +0,0 @@
-package com.beixiao.system.repository;
-
-import com.beixiao.common.BaseDao;
-import com.beixiao.system.domain.AnrongShareInfo;
-
-public interface AnrongShareInfoDao extends BaseDao<AnrongShareInfo>{
-	
-}
