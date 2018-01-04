8e51b103d1a79fad179114ac81fd1d60e7766427
wangqingfan <m18500961204@163.com>
2018-01-04 12:25:57 +0800

    修改？

--- wxapps-common/src/main/java/com/beixiao/shop/service/ShopService.java
+++ wxapps-common/src/main/java/com/beixiao/shop/service/ShopService.java
@@ -1,10 +1,8 @@
 package com.beixiao.shop.service;
 
-import com.beixiao.shop.domain.Shop;
 import com.beixiao.shop.dto.BasicInfo;
 
 public interface ShopService {
-	
 	/**
 	 * 获取基本信息
 	 * @author wqf V1.0 2017年12月25日 下午5:10:00
@@ -12,12 +10,4 @@ public interface ShopService {
 	 * @return BasicInfo
 	 */
 	BasicInfo findBasicInfoByAppId(Integer miniAppId);
-	
-	/**
-	 * 通过id查找
-	 * @author wqf V1.0 2018年1月4日 上午10:33:27
-	 * @param shopId
-	 * @return Shop
-	 */
-	Shop findById(Integer shopId);
 }
